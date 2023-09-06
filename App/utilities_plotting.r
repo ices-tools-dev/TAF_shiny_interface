ba88bfc63363fb6ba045029dee6466d8b510c49b
Luca Lamoni <lucalamoni@gmail.com>
2023-09-06 12:27:52 +0200

    tidied up function

--- App/utilities_plotting.r
+++ App/utilities_plotting.r
@@ -37,53 +37,22 @@ TAFStatsPlot <- function(TAFStatistics, category, percentage) {
 
 
 EGStatsPlot <- function(EGStatistic) {
-    # TAFStatistics$TotStocks <- TAFStatistics$stocks + TAFStatistics$taF_Stocks
-    # TAFStatistics$stocks_perc <- TAFStatistics$stocks/TAFStatistics$TotStocks * 100
-    # TAFStatistics$taF_stocks_perc <- TAFStatistics$taF_Stocks/TAFStatistics$TotStocks * 100
-        # fig <- EGStatistic %>% count(expertGroup, total)
-        # fig <- fig %>% plot_ly(x = ~expertGroup, y = ~total, color = ~year)
+    g <- highlight_key(EGStatistic, ~expertGroup)
 
-        # fig <- plot_ly(EGStatistic, x = ~year,y = ~ percent, mode = 'lines', color = ~expertGroup)
+    p <- plot_ly(g) %>%
+        group_by(expertGroup) %>%
+        add_trace(x = ~year, 
+                    y = ~percent, 
+                    color = ~expertGroup, 
+                    mode = 'lines+markers', 
+                    line = list(shape = 'spline', 
+                                smoothing = .9, 
+                                width = 4), 
+                    marker = list(size = 8)) %>%
+        layout(xaxis = list(title = "")) %>%
+        highlight(on = "plotly_hover", 
+                    selected = attrs_selected(showlegend = FALSE))
 
-        # p <- ggplot(EGStatistic, aes(year, percent, group = expertGroup)) + geom_line()
-        # fig <- ggplotly(p, tooltip = "city") %>%
-        #         highlight(on = "plotly_hover", color = "red")
-        
-g <- highlight_key(EGStatistic, ~expertGroup)
-
-
-p <- plot_ly(g) %>%
-  group_by(expertGroup) %>%
-  add_trace(x = ~year, y = ~percent, color = ~expertGroup, mode = 'lines+markers', line = list(shape = 'spline', smoothing = .9, width = 4), marker = list(size = 8)) %>%
-  layout(xaxis = list(title = "")) %>%
-  highlight(selected = attrs_selected(showlegend = FALSE))
-
-    # if (isTRUE(percentage)) {
-    #     fig2 <- plot_ly(
-    #         data = TAFStatistics %>% filter(categories == category),
-    #         x = ~year,
-    #         y = ~stocks_perc,
-    #         type = "bar",
-    #         name = "Assessments not in TAF"
-    #     )
-    #     fig1 <- fig1 %>% add_trace(
-    #         y = ~taF_stocks_perc,
-    #         name = "Assessments in TAF"
-    #     )
-    #     fig1 <- fig1 %>% layout(yaxis = list(title = "% of stocks"), barmode = "stack")
-    # # } else {
-    #     fig1 <- plot_ly(
-    #         data = TAFStatistics %>% filter(categories == category),
-    #         x = ~year,
-    #         y = ~stocks,
-    #         type = "bar",
-    #         name = "Assessments not in TAF"
-    #     )
-    #     fig1 <- fig1 %>% add_trace(
-    #         y = ~taF_Stocks,
-    #         name = "Assessments in TAF"
-    #     )
-    #     fig1 <- fig1 %>% layout(yaxis = list(title = "N. of stocks"), barmode = "stack")
-    # # }
+    
     p
 }
